7439b092bcb13d8f482e0e7d1e085ba6dacc66ae
Yay295 <Yay295@gmail.com>
2024-04-21 09:54:18 -0500

    use mode structs in encode.c and decode.c

--- src/encode.c
+++ src/encode.c
@@ -357,14 +357,14 @@ static PyTypeObject ImagingEncoderType = {
 /* -------------------------------------------------------------------- */
 
 int
-get_packer(ImagingEncoderObject *encoder, const char *mode, const char *rawmode) {
+get_packer(ImagingEncoderObject *encoder, const Mode *mode, const RawMode *rawmode) {
     int bits;
     ImagingShuffler pack;
 
     pack = ImagingFindPacker(mode, rawmode, &bits);
     if (!pack) {
         Py_DECREF(encoder);
-        PyErr_Format(PyExc_ValueError, "No packer found from %s to %s", mode, rawmode);
+        PyErr_Format(PyExc_ValueError, "No packer found from %s to %s", mode->name, rawmode->name);
         return -1;
     }
 
@@ -400,11 +400,11 @@ PyObject *
 PyImaging_GifEncoderNew(PyObject *self, PyObject *args) {
     ImagingEncoderObject *encoder;
 
-    char *mode;
-    char *rawmode;
+    char *mode_name;
+    char *rawmode_name;
     Py_ssize_t bits = 8;
     Py_ssize_t interlace = 0;
-    if (!PyArg_ParseTuple(args, "ss|nn", &mode, &rawmode, &bits, &interlace)) {
+    if (!PyArg_ParseTuple(args, "ss|nn", &mode_name, &rawmode_name, &bits, &interlace)) {
         return NULL;
     }
 
@@ -412,6 +412,9 @@ PyImaging_GifEncoderNew(PyObject *self, PyObject *args) {
     if (encoder == NULL) {
         return NULL;
     }
+
+    const Mode * const mode = findMode(mode_name);
+    const RawMode * const rawmode = findRawMode(rawmode_name);
 
     if (get_packer(encoder, mode, rawmode) < 0) {
         return NULL;
@@ -433,11 +436,11 @@ PyObject *
 PyImaging_PcxEncoderNew(PyObject *self, PyObject *args) {
     ImagingEncoderObject *encoder;
 
-    char *mode;
-    char *rawmode;
+    char *mode_name;
+    char *rawmode_name;
     Py_ssize_t bits = 8;
 
-    if (!PyArg_ParseTuple(args, "ss|n", &mode, &rawmode, &bits)) {
+    if (!PyArg_ParseTuple(args, "ss|n", &mode_name, &rawmode_name, &bits)) {
         return NULL;
     }
 
@@ -445,6 +448,9 @@ PyImaging_PcxEncoderNew(PyObject *self, PyObject *args) {
     if (encoder == NULL) {
         return NULL;
     }
+
+    const Mode * const mode = findMode(mode_name);
+    const RawMode * const rawmode = findRawMode(rawmode_name);
 
     if (get_packer(encoder, mode, rawmode) < 0) {
         return NULL;
@@ -463,12 +469,12 @@ PyObject *
 PyImaging_RawEncoderNew(PyObject *self, PyObject *args) {
     ImagingEncoderObject *encoder;
 
-    char *mode;
-    char *rawmode;
+    char *mode_name;
+    char *rawmode_name;
     Py_ssize_t stride = 0;
     Py_ssize_t ystep = 1;
 
-    if (!PyArg_ParseTuple(args, "ss|nn", &mode, &rawmode, &stride, &ystep)) {
+    if (!PyArg_ParseTuple(args, "ss|nn", &mode_name, &rawmode_name, &stride, &ystep)) {
         return NULL;
     }
 
@@ -476,6 +482,9 @@ PyImaging_RawEncoderNew(PyObject *self, PyObject *args) {
     if (encoder == NULL) {
         return NULL;
     }
+
+    const Mode * const mode = findMode(mode_name);
+    const RawMode * const rawmode = findRawMode(rawmode_name);
 
     if (get_packer(encoder, mode, rawmode) < 0) {
         return NULL;
@@ -497,11 +506,11 @@ PyObject *
 PyImaging_TgaRleEncoderNew(PyObject *self, PyObject *args) {
     ImagingEncoderObject *encoder;
 
-    char *mode;
-    char *rawmode;
+    char *mode_name;
+    char *rawmode_name;
     Py_ssize_t ystep = 1;
 
-    if (!PyArg_ParseTuple(args, "ss|n", &mode, &rawmode, &ystep)) {
+    if (!PyArg_ParseTuple(args, "ss|n", &mode_name, &rawmode_name, &ystep)) {
         return NULL;
     }
 
@@ -509,6 +518,9 @@ PyImaging_TgaRleEncoderNew(PyObject *self, PyObject *args) {
     if (encoder == NULL) {
         return NULL;
     }
+
+    const Mode * const mode = findMode(mode_name);
+    const RawMode * const rawmode = findRawMode(rawmode_name);
 
     if (get_packer(encoder, mode, rawmode) < 0) {
         return NULL;
@@ -534,7 +546,7 @@ PyImaging_XbmEncoderNew(PyObject *self, PyObject *args) {
         return NULL;
     }
 
-    if (get_packer(encoder, "1", "1;R") < 0) {
+    if (get_packer(encoder, IMAGING_MODE_1, IMAGING_RAWMODE_1_R) < 0) {
         return NULL;
     }
 
@@ -555,8 +567,8 @@ PyObject *
 PyImaging_ZipEncoderNew(PyObject *self, PyObject *args) {
     ImagingEncoderObject *encoder;
 
-    char *mode;
-    char *rawmode;
+    char *mode_name;
+    char *rawmode_name;
     Py_ssize_t optimize = 0;
     Py_ssize_t compress_level = -1;
     Py_ssize_t compress_type = -1;
@@ -565,8 +577,8 @@ PyImaging_ZipEncoderNew(PyObject *self, PyObject *args) {
     if (!PyArg_ParseTuple(
             args,
             "ss|nnny#",
-            &mode,
-            &rawmode,
+            &mode_name,
+            &rawmode_name,
             &optimize,
             &compress_level,
             &compress_type,
@@ -594,6 +606,9 @@ PyImaging_ZipEncoderNew(PyObject *self, PyObject *args) {
         return NULL;
     }
 
+    const Mode * const mode = findMode(mode_name);
+    const RawMode * const rawmode = findRawMode(rawmode_name);
+
     if (get_packer(encoder, mode, rawmode) < 0) {
         free(dictionary);
         return NULL;
@@ -602,7 +617,7 @@ PyImaging_ZipEncoderNew(PyObject *self, PyObject *args) {
     encoder->encode = ImagingZipEncode;
     encoder->cleanup = ImagingZipEncodeCleanup;
 
-    if (rawmode[0] == 'P') {
+    if (rawmode == IMAGING_RAWMODE_P || rawmode == IMAGING_RAWMODE_PA) {
         /* disable filtering */
         ((ZIPSTATE *)encoder->state.context)->mode = ZIP_PNG_PALETTE;
     }
@@ -631,8 +646,8 @@ PyObject *
 PyImaging_LibTiffEncoderNew(PyObject *self, PyObject *args) {
     ImagingEncoderObject *encoder;
 
-    char *mode;
-    char *rawmode;
+    char *mode_name;
+    char *rawmode_name;
     char *compname;
     char *filename;
     Py_ssize_t fp;
@@ -654,8 +669,8 @@ PyImaging_LibTiffEncoderNew(PyObject *self, PyObject *args) {
     if (!PyArg_ParseTuple(
             args,
             "sssnsOO",
-            &mode,
-            &rawmode,
+            &mode_name,
+            &rawmode_name,
             &compname,
             &fp,
             &filename,
@@ -690,6 +705,9 @@ PyImaging_LibTiffEncoderNew(PyObject *self, PyObject *args) {
     if (encoder == NULL) {
         return NULL;
     }
+
+    const Mode * const mode = findMode(mode_name);
+    const RawMode * const rawmode = findRawMode(rawmode_name);
 
     if (get_packer(encoder, mode, rawmode) < 0) {
         return NULL;
@@ -1036,8 +1054,8 @@ PyObject *
 PyImaging_JpegEncoderNew(PyObject *self, PyObject *args) {
     ImagingEncoderObject *encoder;
 
-    char *mode;
-    char *rawmode;
+    char *mode_name;
+    char *rawmode_name;
     Py_ssize_t quality = 0;
     Py_ssize_t progressive = 0;
     Py_ssize_t smooth = 0;
@@ -1061,8 +1079,8 @@ PyImaging_JpegEncoderNew(PyObject *self, PyObject *args) {
     if (!PyArg_ParseTuple(
             args,
             "ss|nnnnpnnnnnnOz#y#y#",
-            &mode,
-            &rawmode,
+            &mode_name,
+            &rawmode_name,
             &quality,
             &progressive,
             &smooth,
@@ -1089,11 +1107,14 @@ PyImaging_JpegEncoderNew(PyObject *self, PyObject *args) {
         return NULL;
     }
 
+    const Mode * const mode = findMode(mode_name);
+    const RawMode * rawmode = findRawMode(rawmode_name);
+
     // libjpeg-turbo supports different output formats.
     // We are choosing Pillow's native format (3 color bytes + 1 padding)
     // to avoid extra conversion in Pack.c.
-    if (ImagingJpegUseJCSExtensions() && strcmp(rawmode, "RGB") == 0) {
-        rawmode = "RGBX";
+    if (ImagingJpegUseJCSExtensions() && rawmode == IMAGING_RAWMODE_RGB) {
+        rawmode = IMAGING_RAWMODE_RGBX;
     }
 
     if (get_packer(encoder, mode, rawmode) < 0) {
@@ -1150,29 +1171,27 @@ PyImaging_JpegEncoderNew(PyObject *self, PyObject *args) {
 
     encoder->encode = ImagingJpegEncode;
 
-    strncpy(((JPEGENCODERSTATE *)encoder->state.context)->rawmode, rawmode, 8);
-
-    ((JPEGENCODERSTATE *)encoder->state.context)->keep_rgb = keep_rgb;
-    ((JPEGENCODERSTATE *)encoder->state.context)->quality = quality;
-    ((JPEGENCODERSTATE *)encoder->state.context)->qtables = qarrays;
-    ((JPEGENCODERSTATE *)encoder->state.context)->qtablesLen = qtablesLen;
-    ((JPEGENCODERSTATE *)encoder->state.context)->subsampling = subsampling;
-    ((JPEGENCODERSTATE *)encoder->state.context)->progressive = progressive;
-    ((JPEGENCODERSTATE *)encoder->state.context)->smooth = smooth;
-    ((JPEGENCODERSTATE *)encoder->state.context)->optimize = optimize;
-    ((JPEGENCODERSTATE *)encoder->state.context)->streamtype = streamtype;
-    ((JPEGENCODERSTATE *)encoder->state.context)->xdpi = xdpi;
-    ((JPEGENCODERSTATE *)encoder->state.context)->ydpi = ydpi;
-    ((JPEGENCODERSTATE *)encoder->state.context)->restart_marker_blocks =
-        restart_marker_blocks;
-    ((JPEGENCODERSTATE *)encoder->state.context)->restart_marker_rows =
-        restart_marker_rows;
-    ((JPEGENCODERSTATE *)encoder->state.context)->comment = comment;
-    ((JPEGENCODERSTATE *)encoder->state.context)->comment_size = comment_size;
-    ((JPEGENCODERSTATE *)encoder->state.context)->extra = extra;
-    ((JPEGENCODERSTATE *)encoder->state.context)->extra_size = extra_size;
-    ((JPEGENCODERSTATE *)encoder->state.context)->rawExif = rawExif;
-    ((JPEGENCODERSTATE *)encoder->state.context)->rawExifLen = rawExifLen;
+    JPEGENCODERSTATE *jpeg_encoder_state = (JPEGENCODERSTATE *)encoder->state.context;
+    jpeg_encoder_state->rawmode = rawmode;
+    jpeg_encoder_state->keep_rgb = keep_rgb;
+    jpeg_encoder_state->quality = quality;
+    jpeg_encoder_state->qtables = qarrays;
+    jpeg_encoder_state->qtablesLen = qtablesLen;
+    jpeg_encoder_state->subsampling = subsampling;
+    jpeg_encoder_state->progressive = progressive;
+    jpeg_encoder_state->smooth = smooth;
+    jpeg_encoder_state->optimize = optimize;
+    jpeg_encoder_state->streamtype = streamtype;
+    jpeg_encoder_state->xdpi = xdpi;
+    jpeg_encoder_state->ydpi = ydpi;
+    jpeg_encoder_state->restart_marker_blocks = restart_marker_blocks;
+    jpeg_encoder_state->restart_marker_rows = restart_marker_rows;
+    jpeg_encoder_state->comment = comment;
+    jpeg_encoder_state->comment_size = comment_size;
+    jpeg_encoder_state->extra = extra;
+    jpeg_encoder_state->extra_size = extra_size;
+    jpeg_encoder_state->rawExif = rawExif;
+    jpeg_encoder_state->rawExifLen = rawExifLen;
 
     return (PyObject *)encoder;
 }
